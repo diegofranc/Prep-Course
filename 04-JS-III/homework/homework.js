cb2f2bf840b3a28f0c7abc336827c3966dc5db5d
diegofranc <diegofranco2001@gmail.com>
2022-03-06 22:27:12 -0300

    04-JS listo

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -124,6 +124,14 @@ function multiplicarArgumentos() {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
   // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
   // Escribe tu código aquí:
+var arg = 1;
+
+  if (arguments.length==0)
+  return 0;
+  else
+  for (let i=0; i<arguments.length;i++){
+    arg=arg*arguments[i];
+} return arg;
 
 }
 
@@ -131,7 +139,11 @@ function multiplicarArgumentos() {
 function cuentoElementos(arreglo){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
-
+var contando = 0;
+for (let i=0; i<arreglo.length;i++){
+  if (arreglo[i]>18)
+  contando=contando+1;
+} return contando;
 }
 
 
@@ -152,7 +164,7 @@ function empiezaConNueve(n) {
   //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
-  var num = toString(n);
+  var num = String(n);
   if (num[0]=='9')
   return true;
   else 
@@ -165,9 +177,10 @@ function todosIguales(arreglo) {
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
   //Escribe tu código aquí  
-  var verif = '';
+  var verif = arreglo[0];
   var resul = true;
-  for (let i= 0;i<arreglo.length;i++){
+  for (let i= 1;i<arreglo.length;i++){
+    
     if (verif!==arreglo[i])
     resul = false;
 
@@ -181,6 +194,23 @@ function mesesDelAño(array) {
   // "Enero", "Marzo" y "Noviembre", guardarlo en nuevo array y retornarlo.
   //Si alguno de los meses no está, devolver: "No se encontraron los meses pedidos"
   // Tu código:
+  var nuevoarray=[];
+  for (let i=0;i<12;i++){
+    if (array[i]=="Marzo")
+    nuevoarray.push(array[i]);
+  }
+  for (let j=0;j<12;j++){
+    if (array[j]=="Noviembre")
+    nuevoarray.push(array[j]);
+  }
+  for (let k=0;k<12;k++){
+    if (array[k]=="Enero")
+    nuevoarray.push(array[k]);
+  }
+  if (nuevoarray.length<3)
+  return "No se encontraron los meses pedidos";
+  else
+  return nuevoarray;
 }
 
 
@@ -204,6 +234,19 @@ function breakStatement(numero) {
   //devolver: "Se interrumpió la ejecución"
   //Pista: usá el statement 'break'
   // Tu código:
+  var miarray=[];
+  var minum = 0
+  for (let i=1;i<=10;i++){
+  if (i==minum)
+  break;
+  else
+  minum= numero+2*i;
+  miarray.push(minum);
+  }
+  if (miarray.length<10)
+  return "Se interrumpió la ejecución";
+  else  
+  return miarray;
 }
 
 
@@ -214,6 +257,15 @@ function continueStatement(numero) {
   //Cuando el número de iteraciones alcance el valor 5, no se suma en ese caso y se continua con la siguiente iteración
   //Pista: usá el statement 'continue'
   // Tu código:
+  var miarray=[];
+  for (let i=1;i<=10;i++){
+    if (i===5)    {continue;}
+numero= numero+2;
+miarray.push(numero);
+   
+  }
+  return miarray;
+
 }
 
 
